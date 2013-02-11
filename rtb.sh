35d99e197251f92f81e0a16958f082448b2f64a4
Robert Cotey <coteyr@coteyr.net>
2013-02-11 06:58:13 -0500

    Finally working port test

--- rtb.sh
+++ rtb.sh
@@ -41,13 +41,13 @@ function ftbmon() {
 }
 
 function server_check() {
-  if [[ "$(ps faux | grep "${server_start}" | grep -i screen)" == "" ]] then
+  if [[ "$(ps faux | grep "${server_start}" | grep -i screen)" == "" ]]; then
     log "Server NOT running", "Attempting to start."
     server_restart
-  elif [[ $(tail -n1 $crashlog | grep CREATE) ]] then
+  elif [[ $(tail -n1 $crashlog | grep CREATE) ]]; then
     log "Server crash detected", "Attempting to restart."
     server_restart
-  elif [[ $([ echo >/dev/tcp/127.0.0.1/25565 ] && echo "open") <> "open"]] then
+  elif [[ "$(cat test.cmd | netcat 127.0.0.1 25565)"  == "" ]]; then
     log "Server port not responding", "Attempting to restart."
     server_restart
   fi
